ccde58c8a1e407deafd53d7b74e6368901253d3c
Luis C <luiscassih@gmail.com>
2025-01-29 03:24:39 -0300

    bugfix with o and add or insert in Kak mode

--- lua/AniKakoune/init.lua
+++ lua/AniKakoune/init.lua
@@ -9,7 +9,7 @@ M.setup = function(config)
   local opts = config or {}
   local word = opts.word_keys or { "w", "b", "e", "W", "B", "E" }
   local disableKak = opts.disable_keys or {
-    "i", "a", "I", "A",
+    "i", "a", "I", "A", "o", "O",
     "h", "j", "k", "l", "f", "F", "t", "T", "/", "?", "n", "N",
     "gg", "G", "$", "_", "0",
     "H", "L", "J", "K",
@@ -57,7 +57,11 @@ M.setup = function(config)
         end
         vim.cmd("normal! v" .. k)
       else
-        vim.api.nvim_feedkeys(vim.v.count1 .. k, "n", true)
+        if vim.v.count > 1 then
+          vim.api.nvim_feedkeys(vim.v.count .. k, "n", true)
+        else
+          vim.api.nvim_feedkeys(k, "n", true)
+        end
       end
     end)
   end
@@ -67,20 +71,19 @@ M.setup = function(config)
     vim.keymap.set('v', k, function()
       if (kakActive) then
         kakActive = false
-        -- vim.api.nvim_input("<Esc>")
-        -- vim.cmd("normal! v")
-        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "n", true)
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true) .. k, "n", true)
+      else
+        vim.cmd("normal! " .. vim.v.count1 .. k)
       end
-      vim.api.nvim_feedkeys(vim.v.count1 .. k, "n", true)
-    end)
+    end, { nowait = true, expr = false })
   end
   vim.keymap.set('v', 'v', function()
     if (kakActive) then
       kakActive = false
     else
-      vim.api.nvim_feedkeys("v", "n", true)
+      vim.cmd("normal! v")
     end
-  end)
+  end, { nowait = true })
 
   vim.api.nvim_create_autocmd('ModeChanged', {
     pattern = '*',
