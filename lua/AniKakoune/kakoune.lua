988682a2e524dfbbb599b9eaeea695d126bde825
Luis C <luiscassih@gmail.com>
2025-02-04 18:32:47 -0300

    indentation and cleaning

--- lua/AniKakoune/kakoune.lua
+++ lua/AniKakoune/kakoune.lua
@@ -97,7 +97,6 @@ M.word_move = function(target, count)
             if current_type == CharacterType.EndOfLine
               or prev_type == CharacterType.EndOfLine
             then
-              print("heeere")
               -- To ignore empty lines
               current_pos[1] = current_pos[1] - 1
               line = current_pos[1]
@@ -110,7 +109,6 @@ M.word_move = function(target, count)
             end
 
             if (current_type == CharacterType.Punctuation and prev_type ~= CharacterType.Punctuation)
-              -- or (current_type == CharacterType.WhiteSpace and prev_type ~= CharacterType.WhiteSpace)
               or (current_type == CharacterType.Word and prev_type ~= CharacterType.Word)
             then
               hl_start[2] = hl_start[2] - 1
@@ -127,129 +125,126 @@ M.word_move = function(target, count)
         ::continue::
       end
     else
-    while true do
-      -- print("evaluating for pos", vim.inspect(current_pos), vim.inspect(line_content))
-      local current_char = get_current_char(current_pos, line_content)
-      local current_type = get_character_type(current_char)
+      while true do
+        local current_char = get_current_char(current_pos, line_content)
+        local current_type = get_character_type(current_char)
 
-      local next_char = get_next_char(current_pos, line_content)
-      local next_type = get_character_type(next_char)
+        local next_char = get_next_char(current_pos, line_content)
+        local next_type = get_character_type(next_char)
 
-      -- print("evaluating:", current_char, current_type, next_char, next_type)
-      if hl_start[2] ~= current_pos[2] then
-        -- We already started highlighting the word, so we check if we are at the end of the word
-        if next_type == CharacterType.EndOfLine then break end
-
-        if target == M.Targets.NextWordStart then
-          if (current_type == CharacterType.WhiteSpace and next_type ~= CharacterType.WhiteSpace)
-            or (current_type == CharacterType.Punctuation and next_type == CharacterType.Word)
-            or (current_type ~= CharacterType.Punctuation and next_type == CharacterType.Punctuation)
-          then
-            break
-          end
-        end
-
-        if target == M.Targets.NextLongWordStart then
-          if (current_type == CharacterType.WhiteSpace and next_type ~= CharacterType.WhiteSpace)
-          then
-            break
-          end
-        end
-
-        if target == M.Targets.NextWordEnd then
-          if (current_type == CharacterType.Word and next_type ~= CharacterType.Word)
-            or (current_type ~= CharacterType.Punctuation and next_type == CharacterType.Punctuation)
-            or (current_type == CharacterType.Punctuation and next_type ~= CharacterType.Punctuation)
-          then
-            break
-          end
-        end
-
-        if target == M.Targets.NextLongWordEnd then
-          if (current_type ~= CharacterType.WhiteSpace and next_type == CharacterType.WhiteSpace)
-          then
-            break
-          end
-        end
-      else
-        -- start == current, we start highlighting the word
-        if moved_from_original == true then
-          -- we already skipped the first character, for example, when we start at the space before a word
-          -- so we don't need to keep skipping, for example, in " ((a"
-          -- we start in space, we keep and moved_from_original is true, so we highlight all the non-word
-          -- characters until we hit a word, so the result is "((" being highlighted
+        if hl_start[2] ~= current_pos[2] then
+          -- We already started highlighting the word, so we check if we are at the end of the word
           if next_type == CharacterType.EndOfLine then break end
+
           if target == M.Targets.NextWordStart then
-            if (current_type ~= CharacterType.Punctuation and next_type == CharacterType.Punctuation)
+            if (current_type == CharacterType.WhiteSpace and next_type ~= CharacterType.WhiteSpace)
               or (current_type == CharacterType.Punctuation and next_type == CharacterType.Word)
+              or (current_type ~= CharacterType.Punctuation and next_type == CharacterType.Punctuation)
             then
               break
-            end
-          end
-
-          -- NextLongWordStart doesn't need because punctuation and word are equal
-
-          if target == M.Targets.NextWordEnd then
-            if (current_type == CharacterType.Word and next_type ~= CharacterType.Word)
-              or (current_type == CharacterType.Punctuation and next_type ~= CharacterType.Punctuation)
-            then
-              break
-            end
-          end
-
-        else
-          -- This is the first block to execute, we just started at the original, and didn't moved
-          if current_type == CharacterType.EndOfLine
-            or next_type == CharacterType.EndOfLine
-          then
-            -- To ignore empty lines
-            current_pos[1] = current_pos[1] + 1
-            current_pos[2] = 1
-            hl_start = {current_pos[1], current_pos[2]}
-            hl_end = {current_pos[1], current_pos[2]}
-            line = current_pos[1]
-            line_content = vim.fn.getline(line)
-            goto continue
-          end
-
-          if target == M.Targets.NextWordStart then
-            if (current_type ~= CharacterType.Punctuation and next_type == CharacterType.Punctuation)
-              or (current_type == CharacterType.WhiteSpace and next_type == CharacterType.Word)
-            then
-              hl_start[2] = hl_start[2] + 1
-              moved_from_original = true
             end
           end
 
           if target == M.Targets.NextLongWordStart then
             if (current_type == CharacterType.WhiteSpace and next_type ~= CharacterType.WhiteSpace)
             then
-              hl_start[2] = hl_start[2] + 1
-              moved_from_original = true
+              break
             end
           end
 
-          if target == M.Targets.NextWordEnd or target == M.Targets.NextLongWordEnd then
+          if target == M.Targets.NextWordEnd then
             if (current_type == CharacterType.Word and next_type ~= CharacterType.Word)
+              or (current_type ~= CharacterType.Punctuation and next_type == CharacterType.Punctuation)
               or (current_type == CharacterType.Punctuation and next_type ~= CharacterType.Punctuation)
             then
-              hl_start[2] = hl_start[2] + 1
-              moved_from_original = true
+              break
             end
           end
-        end
-      end -- end else start == curent
 
-      current_pos[2] = current_pos[2] + 1
-      if current_pos[2] > #line_content then
-        break
-      end
-    ::continue::
-    end -- while
+          if target == M.Targets.NextLongWordEnd then
+            if (current_type ~= CharacterType.WhiteSpace and next_type == CharacterType.WhiteSpace)
+            then
+              break
+            end
+          end
+        else
+          -- start == current, we start highlighting the word
+          if moved_from_original == true then
+            -- we already skipped the first character, for example, when we start at the space before a word
+            -- so we don't need to keep skipping, for example, in " ((a"
+            -- we start in space, we keep and moved_from_original is true, so we highlight all the non-word
+            -- characters until we hit a word, so the result is "((" being highlighted
+            if next_type == CharacterType.EndOfLine then break end
+            if target == M.Targets.NextWordStart then
+              if (current_type ~= CharacterType.Punctuation and next_type == CharacterType.Punctuation)
+                or (current_type == CharacterType.Punctuation and next_type == CharacterType.Word)
+              then
+                break
+              end
+            end
+
+            -- NextLongWordStart doesn't need because punctuation and word are equal
+
+            if target == M.Targets.NextWordEnd then
+              if (current_type == CharacterType.Word and next_type ~= CharacterType.Word)
+                or (current_type == CharacterType.Punctuation and next_type ~= CharacterType.Punctuation)
+              then
+                break
+              end
+            end
+
+          else
+            -- This is the first block to execute, we just started at the original, and didn't moved
+            if current_type == CharacterType.EndOfLine
+              or next_type == CharacterType.EndOfLine
+            then
+              -- To ignore empty lines
+              current_pos[1] = current_pos[1] + 1
+              current_pos[2] = 1
+              hl_start = {current_pos[1], current_pos[2]}
+              hl_end = {current_pos[1], current_pos[2]}
+              line = current_pos[1]
+              line_content = vim.fn.getline(line)
+              goto continue
+            end
+
+            if target == M.Targets.NextWordStart then
+              if (current_type ~= CharacterType.Punctuation and next_type == CharacterType.Punctuation)
+                or (current_type == CharacterType.WhiteSpace and next_type == CharacterType.Word)
+              then
+                hl_start[2] = hl_start[2] + 1
+                moved_from_original = true
+              end
+            end
+
+            if target == M.Targets.NextLongWordStart then
+              if (current_type == CharacterType.WhiteSpace and next_type ~= CharacterType.WhiteSpace)
+              then
+                hl_start[2] = hl_start[2] + 1
+                moved_from_original = true
+              end
+            end
+
+            if target == M.Targets.NextWordEnd or target == M.Targets.NextLongWordEnd then
+              if (current_type == CharacterType.Word and next_type ~= CharacterType.Word)
+                or (current_type == CharacterType.Punctuation and next_type ~= CharacterType.Punctuation)
+              then
+                hl_start[2] = hl_start[2] + 1
+                moved_from_original = true
+              end
+            end
+          end
+        end -- end else start == curent
+
+        current_pos[2] = current_pos[2] + 1
+        if current_pos[2] > #line_content then
+          break
+        end
+      ::continue::
+      end -- while
     end -- if prev
   hl_end = {current_pos[1], current_pos[2]}
   end -- for
-  -- print("start", vim.inspect(hl_start), "end", vim.inspect(hl_end))
   return { hl_start, hl_end }
 end
 return M
